bc8ceef8edde8078bc28e6160c827b42530a4f70
Raphael <raphaxd18@gmail.com>
2022-09-08 18:18:29 -0300

    Tentando criar uma logica com arrays e falhando miseravelmente

--- Aula_05/5_aluraflix/main.js
+++ Aula_05/5_aluraflix/main.js
@@ -1,23 +1,37 @@
-function adicionarFilme() {
-    var enderecoFilme = document.getElementById("endFilme").value;
+/* function adicionarFilme() {
+
     var nomeFilme = document.getElementById("nomeFilme").value;
+    var enderecoFilme = document.getElementById("endFilme").value;
+
+    console.log(nomeFilme);
+    console.log(enderecoFilme)
 
     if (enderecoFilme.endsWith(".jpg" || ".png")) {
-        listarFilmesNaTela(enderecoFilme, nomeFilme);
+        listarFilmesNaTela(nomeFilme, enderecoFilme);
     } else {
         console.error("Endereço de filme invalido");
     }
-    document.getElementById("endFilme").value = "";
+    document.getElementById("endFilme").value = ""; 
 }
 
-function listarFilmesNaTela(endFilme, nomeFilme) {
+ function listarFilmesNaTela(nomeFilme, endFilme) {
     var elementoEnderecoFilme = "<img src=" + endFilme + ">" + nomeFilme;
     var elementoListaFilmes = document.getElementById("listaFilmes");
 
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoEnderecoFilme;
-}
+}*/
 
-function removerFilme() {
-    
+var nomeFilme = [{nome:'test', endereco:'teste'},{nome: 'test1', endereco: 'teste1'}] ;
+
+console.log(nomeFilme)
+function adicionarFilme() {
+
+    nomeFilme<{ 
+        nome: string,
+        endereco: string 
+    }>push(...items, {
+        nome: (document.getElementById("nomeFilme").value),
+        endereco: (document.getElementById("endFilme").value),
+    })
 }
 
