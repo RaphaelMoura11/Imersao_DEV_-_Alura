bad73e3f02aa8acbf25a0d857ebdcb99efac0653
Raphael <raphaxd18@gmail.com>
2022-09-19 19:59:25 -0300

    missão array completa e alteração no design.

--- Aula_05/5_aluraflix/main.js
+++ Aula_05/5_aluraflix/main.js
@@ -1,3 +1,4 @@
+//modo feito na alura
 /*  function adicionarFilme() {
 
     var nomeFilme = document.getElementById("nomeFilme").value;
@@ -21,20 +22,22 @@
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoEnderecoFilme;
 }*/ 
 
+
+//Desafio
  var dadosFilme = {
     nomefilme: [],
     enderecofilme: []
 }
 
 function adicionarFilme() {
-    var endereco = document.getElementById("endFilme").value;
     var nome = document.getElementById("nomeFilme").value;
+    var endereco = document.getElementById("endFilme").value;
 
-    //Verifica se a imagem é jpg e envia para os arrays correspondentes.
+    //Verifica se a imagem é jpg e envia para o array.
     if (endereco.endsWith(".jpg")) {
 
-        dadosFilme.nomefilme.push(nome)
-        dadosFilme.enderecofilme.push(endereco)
+        dadosFilme.nomefilme.push(nome);
+        dadosFilme.enderecofilme.push(endereco);
 
         exibirFilmes(dadosFilme.nomefilme, dadosFilme.enderecofilme);
     } else {
@@ -42,15 +45,28 @@ function adicionarFilme() {
     }
     document.getElementById("nomeFilme").value = "";
     document.getElementById("endFilme").value = "";
-    console.log(dadosFilme)
 }
 
+var i = 0;
 function exibirFilmes(nomeFilme, endFilme) {
     var elementoListaFilmes = document.getElementById("listaFilmes");
-    for (var i = 0; i < endFilme.length; i++) {
-        elementoListaFilmes.innerHTML = "<img src=" + endFilme + "><figcaption> " + nomeFilme + "</figcaption>";
-
-        console.log(nomeFilme)
-        console.log(endFilme)
+    
+    for (; i < nomeFilme.length; i++) {
+        elementoListaFilmes.innerHTML += "<figure><img src=" + endFilme[i] + "><figcaption> " + nomeFilme[i] + "</figcaption></figure>";
     }
+    console.log(nomeFilme);
+    console.log(endFilme);
+}
+
+//função de remoção de filme da lista
+function removerFilme() {
+    var nome = document.getElementById("nomeFilme").value;
+
+    if (nome == dadosFilme.nomefilme) {
+        console.log("teste")
+    } else {
+        console.error("Não é possivel remover item. O filme " + nome + " não existe na lista.")
+    }
+
+    console.log(dadosFilme.nomefilme)
 }
