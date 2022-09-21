335c92483686c4ed5aa84278e00be85d54555c5a
Raphael <raphaxd18@gmail.com>
2022-09-21 16:01:33 -0300

    desafio 3 pronto. adição e remoção de filme pelo array pronto

--- Aula_05/5_aluraflix/main.js
+++ Aula_05/5_aluraflix/main.js
@@ -20,20 +20,20 @@
     var elementoListaFilmes = document.getElementById("listaFilmes");
 
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoEnderecoFilme;
-}*/ 
+}*/
 
 
 //Desafio
- var dadosFilme = {
+var dadosFilme = {
     nomefilme: [],
     enderecofilme: []
 }
 
+//Função que adiciona filme no array
 function adicionarFilme() {
     var nome = document.getElementById("nomeFilme").value;
     var endereco = document.getElementById("endFilme").value;
 
-    //Verifica se a imagem é jpg e envia para o array.
     if (endereco.endsWith(".jpg")) {
 
         dadosFilme.nomefilme.push(nome);
@@ -47,26 +47,28 @@ function adicionarFilme() {
     document.getElementById("endFilme").value = "";
 }
 
-var i = 0;
-function exibirFilmes(nomeFilme, endFilme) {
-    var elementoListaFilmes = document.getElementById("listaFilmes");
-    
-    for (; i < nomeFilme.length; i++) {
-        elementoListaFilmes.innerHTML += "<figure><img src=" + endFilme[i] + "><figcaption> " + nomeFilme[i] + "</figcaption></figure>";
-    }
-    console.log(nomeFilme);
-    console.log(endFilme);
-}
-
-//função de remoção de filme da lista
+//função que remove filme do array
 function removerFilme() {
     var nome = document.getElementById("nomeFilme").value;
 
-    if (nome == dadosFilme.nomefilme) {
-        console.log("teste")
+    if (dadosFilme.nomefilme.indexOf(nome) > -1) {
+        var posicao = dadosFilme.nomefilme.indexOf(nome);
+        dadosFilme.nomefilme.splice(posicao, 1);
+        dadosFilme.enderecofilme.splice(posicao, 1);
+        
+        exibirFilmes(dadosFilme.nomefilme, dadosFilme.enderecofilme);
     } else {
-        console.error("Não é possivel remover item. O filme " + nome + " não existe na lista.")
+        console.error("Não é possivel remover item. O filme " + nome + " não existe na lista.");
     }
+}
 
-    console.log(dadosFilme.nomefilme)
+//Atualiza lista de filmes
+function exibirFilmes(nomeFilme, endFilme) {
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+
+    elementoListaFilmes.innerHTML = '';
+    for (var i = 0; i < nomeFilme.length; i++) {
+        elementoListaFilmes.innerHTML += "<figure><img src=" + endFilme[i] + "><figcaption> " + nomeFilme[i] + "</figcaption></figure>";
+    }
+    console.log(dadosFilme);
 }
