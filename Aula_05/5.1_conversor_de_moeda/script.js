63913da2e94581a2457a7fffcefa884a383acb74
Raphael <raphaxd18@gmail.com>
2022-09-21 18:57:47 -0300

    função de cada moeda pronta

--- Aula_05/5.1_conversor_de_moeda/script.js
+++ Aula_05/5.1_conversor_de_moeda/script.js
@@ -1,55 +1,96 @@
-function Converter() {
+function converter() {
 
-    var moedaOut = document.getElementById("moedasOut");
-    for (var i = 0; i < moedaOut.length; i++) {
+    convertBitcoin()
+}
 
-        if (moedaOut[i].value === "Real") {
-            converteReal();
-        } /* else if (moedaOut[i].value === "Dolar") {
-            converteDolar();
-        } else if (moedaOut[i].value === "Euro") {
-            converteEuro();
-        } else if (moedaOut[i].value === "Bitcoin") {
-            convertBitcoin();
-        } */
-    }
-
-};
-
-//convertendo para real
+//Função que converte o valor para Real
 function converteReal() {
-    var valorElemento = parseFloat(document.getElementById("valor").value);
+    var moedaEntrada = document.getElementById("moedaIn").value;
+    var valorElemento = document.getElementById("valor").value;
 
-    var moedaIn = document.getElementById("moedasIn");
-    for (var i = 0; i < moedaIn.length; i++) {
-        if (moedaIn[i].value === "Dolar") {
-            var X = "5"
-        } else if (moedaIn[i].value === "Euro") {
-            var X = "5.50"
-        } else if (moedaIn[i].value === "Bitcoin") {
-            var X = "108676.26"
-        } else {
-            console.log("erro aki")
-        }
-
-        var valor = valorElemento * X
-        var valorEmReal = valor.toFixed(2)
-
-        var elementoValorConvertido = document.getElementById("valorConvertido")
-        var valorConvertido = "O valor em Real é R$ " + valorEmReal
-        elementoValorConvertido.innerHTML = valorConvertido;
+    if (moedaEntrada === "Dolar") {
+        var X = "5"
+    } else if (moedaEntrada === "Euro"){
+        var X = "5.50"
+    } else if (moedaEntrada === "Bitcoin") {
+        var X = "108676.26"
+    } else {
+     var X = "1"
     }
+
+    var valor = valorElemento * X
+    var valorEmReal = valor.toFixed(2)
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em Real é R$ " + valorEmReal
+    elementoValorConvertido.innerHTML = valorConvertido;
 };
 
+//Função que converte o valor para Dolar
 function converteDolar() {
+    var moedaEntrada = document.getElementById("moedaIn").value;
+    var valorElemento = document.getElementById("valor").value;
 
-    console.log("Dolar");
-};
+    if (moedaEntrada === "Real") {
+        var X = "0.19"
+    } else if (moedaEntrada === "Euro"){
+        var X = "0.98"
+    } else if (moedaEntrada === "Bitcoin") {
+        var X = "18666.50"
+    } else {
+     var X = "1"
+    }
 
+    var valor = valorElemento * X
+    var valorEmDolar = valor.toFixed(2)
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em Dolar é US$ " + valorEmDolar;
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+//Função que converte o valor para Euro
 function converteEuro() {
-    console.log("Euro");
-};
+    var moedaEntrada = document.getElementById("moedaIn").value;
+    var valorElemento = document.getElementById("valor").value;
 
+    if (moedaEntrada === "Real") {
+        var X = "0.2"
+    } else if (moedaEntrada === "Dolar"){
+        var X = "1.02"
+    } else if (moedaEntrada === "Bitcoin") {
+        var X = "18800.08"
+    } else {
+     var X = "1"
+    }
+
+    var valor = valorElemento * X
+    var valorEmEuro = valor.toFixed(2)
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em Euro é € " + valorEmEuro;
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+//Função que converte o valor para Bitcoin
 function convertBitcoin() {
-    console.log("Bitcoin");
-};
+    var moedaEntrada = document.getElementById("moedaIn").value;
+    var valorElemento = document.getElementById("valor").value;
+
+    if (moedaEntrada === "Real") {
+        var X = "0.00001044141177"
+    } else if (moedaEntrada === "Dolar"){
+        var X = "0.00005424682086"
+    } else if (moedaEntrada === "Euro") {
+        var X = "0.00005319130545"
+    } else {
+     var X = "1"
+    }
+
+    var valor = valorElemento * X
+    var valorEmBitcoin = valor.toFixed(2)
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em Bitcoin é " + valorEmBitcoin + " BTC";
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
