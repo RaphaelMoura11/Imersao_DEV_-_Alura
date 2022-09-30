858bfd5b36a612d8653f1012d496b4fcb719f5c9
Raphael <raphaxd18@gmail.com>
2022-09-29 22:58:44 -0300

    empate pra todo mundo pronto. comparador  de partidas com vitoria, empate e derrota incompleta

--- Aula_06/6_tabela_de_classificacao/main.js
+++ Aula_06/6_tabela_de_classificacao/main.js
@@ -1,7 +1,7 @@
 var partidas = 0;
 
-var rapha = { nome: "Rapha", vitorias: 2, empates: 1, derrotas: 1, pontos: 0 };
-var lu = { nome: "Analu", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
+var rapha = { nome: "Rapha", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+var lu = { nome: "Analu", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
 
 function calculaPontos(jogador) {
     var pontos = (jogador.vitorias * 3) + jogador.empates;
@@ -13,6 +13,7 @@ lu.pontos = calculaPontos(lu);
 
 var jogadores = [rapha, lu];
 
+
 function exibirJogadoresTela(jogadores) {
     var elemento = "";
     for (var i = 0; i < jogadores.length; i++) {
@@ -23,6 +24,7 @@ function exibirJogadoresTela(jogadores) {
         elemento += "<td>" + jogadores[i].pontos + "</td>";
         elemento += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
         elemento += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+        elemento += "<td><button onClick='adicionarDerrora(" + i + ")'>Derrota</button></td>";
         elemento += "</tr>";
     }
 
@@ -35,20 +37,40 @@ function exibirJogadoresTela(jogadores) {
 
 exibirJogadoresTela(jogadores)
 
+function verificaPartidas(jogador) {
+    var totalPartidas = jogador.vitorias + jogador.empates + jogador.derrotas;
+    
+    if (totalPartidas === partidas) {
+        jogador.vitorias++;
+        partidas++;
+    }else if (jogador.derrotas > partidas){
+        jogador.vitorias++
+    } else {
+        alert("Numero de partidas diferente dos resultados finais.")
+    }
+}
+
 function adicionarVitoria(i) {
     var jogador = jogadores[i];
-    jogador.vitorias++;
-    partidas++;
+    verificaPartidas(jogador)
     jogador.pontos = calculaPontos(jogador);
-    exibirJogadoresTela(jogadores);
 
-    console.log(partidas)
+    exibirJogadoresTela(jogadores);
 }
 
 function adicionarEmpate(i) {
-    var jogador = jogadores[i];
-    jogador.empates++;
+    for (var i = 0; i < jogadores.length; i++) {
+        var jogador = jogadores[i];
+
+        jogador.empates++;
+        jogador.pontos = calculaPontos(jogador);
+    }
     partidas++;
-    jogador.pontos = calculaPontos(jogador);
+    exibirJogadoresTela(jogadores);
+}
+
+function adicionarDerrora(i) {
+    var jogador = jogadores[i];
+    jogador.derrotas++;
     exibirJogadoresTela(jogadores);
 }
